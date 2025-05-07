6ea0b2996c971b48c4cc20ecfd2e8a13e8ad1b81
missbabelfish <missbabelfish@gmail.com>
2025-05-07 14:08:51 -0500

    move prettyPrint to module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Node, Tree } from "./nodeTree.js";
+import prettyPrint from "./prettyPrint.js";
 
 const newTree =  new Tree([1,2,3,4,5,6,7])
 
@@ -6,26 +7,6 @@ console.log(newTree)
 console.log(newTree.root)
 console.log(newTree.data)
 
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node === null) {
-        return;
-    }
-    if (node.right !== null) {
-        prettyPrint(
-            node.right,
-            `${prefix}${isLeft ? '│   ' : '    '}`,
-            false
-        );
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
-    if (node.left !== null) {
-        prettyPrint(
-            node.left,
-            `${prefix}${isLeft ? '    ' : '│   '}`,
-            true
-        );
-    }
-};
 
 prettyPrint(newTree.root)
 
